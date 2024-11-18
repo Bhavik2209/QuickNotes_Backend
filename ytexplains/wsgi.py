671f73fcf8604573bb30dbfd803dc66f4e77fd27
Bhavik2209 <bhavikrohit22@gmail.com>
2024-11-18 10:08:18 +0530

    dp ch

--- ytexplains/wsgi.py
+++ ytexplains/wsgi.py
@@ -4,8 +4,13 @@ WSGI config for ytexplains project.
 
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.management import execute_from_command_line
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ytexplains.settings')
 
 application = get_wsgi_application()
 app = application  # Add this line for Vercel
+
+if __name__ == "__main__":
+    execute_from_command_line(sys.argv)
